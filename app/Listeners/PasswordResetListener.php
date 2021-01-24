f413b91716aa51deea5313d248468b2a993d8517
Samir Vora <sosolid1236@gmail.com>
2021-01-24 15:05:26 +0000

    Log password reset events

---
+++ app/Listeners/PasswordResetListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Psr\Log\LogLevel;
+
+class PasswordResetListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  PasswordReset $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $user = $event->user;
+        $msg = "User ".$user->email." reset their password";
+        Log::log(LogLevel::INFO, $msg);
+    }
+}
