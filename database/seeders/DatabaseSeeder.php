59eac9afb44ec703c89e77f637f94a305cc7326e
Samir Vora <sosolid1236@gmail.com>
2021-01-26 19:21:10 +0000

    Added IngredientRecipe seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserProfileSeeder::class,
             IngredientSeeder::class,
             RecipeSeeder::class,
+            IngredientRecipeSeeder::class,
         ]);
     }
 }
