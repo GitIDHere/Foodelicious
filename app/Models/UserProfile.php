975f489b381dd997b34b5dd7552cf8a386342607
Samir Vora <sosolid1236@gmail.com>
2021-04-23 19:31:35 +0100

    - Updated the UserProfile seeder to account for the removal of forename and surname - Seed the UserProfile from the User seeder

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -8,27 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class UserProfile extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'username',
-        'forename',
-        'surname',
         'desc',
     ];
-    
-    protected $attributes = [
-        'full_name'  
-    ];
-    
-    /**
-     * @return string
-     */
-    public function getFullNameAttribute()
-    {
-        return $this->forename.' '.$this->surname;
-    }
-    
-    
     /**
      * User profile belongs to one User
      */
@@ -36,7 +20,7 @@ class UserProfile extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
@@ -44,5 +28,5 @@ class UserProfile extends Model
     {
         return $this->hasMany(Recipe::class);
     }
-    
+
 }
